6833c03db94912b7445a71f046881040c91e1870
BigNo219 <bigno@intellieffect.com>
2026-03-16 21:52:25 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,12 +1,36 @@
-const CACHE_NAME = 'couple-diary-v1';
-const urlsToCache = ['/'];
+const CACHE_NAME = 'couple-diary-v1'
+const PRECACHE_URLS = [
+  '/couple-diary/',
+  '/couple-diary/index.html',
+  '/couple-diary/icon-192.svg',
+  '/couple-diary/icon-512.svg',
+]
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-});
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  )
+  self.skipWaiting()
+})
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
+        return response
+      })
+      .catch(() => caches.match(event.request))
+  )
+})
